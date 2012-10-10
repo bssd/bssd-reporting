fd5a733755356bc73e28ea8111ba08d0de5ba434
Andy Stewart <andy.stewart@bssd.co.uk>
2012-10-10 20:08:44 +0100

    Simple generator which takes a directory of csv files and outputs a directory of jpeg charts.

--- bssd-reporting-chart/src/main/java/uk/co/bssd/reporting/chart/JpegChartWriter.java
+++ bssd-reporting-chart/src/main/java/uk/co/bssd/reporting/chart/JpegChartWriter.java
@@ -8,12 +8,16 @@ import org.jfree.chart.JFreeChart;
 
 public class JpegChartWriter implements ChartWriter {
 
-	private final String filename;
+	private final File file;
 	private final int height;
 	private final int width;
 
 	public JpegChartWriter(String filename, int height, int width) {
-		this.filename = filename;
+		this(new File(filename), height, width);
+	}
+
+	public JpegChartWriter(File file, int height, int width) {
+		this.file = file;
 		this.height = height;
 		this.width = width;
 	}
@@ -21,8 +25,8 @@ public class JpegChartWriter implements ChartWriter {
 	@Override
 	public void write(JFreeChart chart) {
 		try {
-			ChartUtilities.saveChartAsJPEG(new File(this.filename), chart,
-					this.height, this.width);
+			ChartUtilities.saveChartAsJPEG(this.file, chart, this.height,
+					this.width);
 		} catch (IOException e) {
 			throw new IllegalStateException("Unable to save chart", e);
 		}
--- bssd-reporting-generator/src/main/java/uk/co/bssd/reporting/generator/SimpleFileSystemGenerator.java
+++ bssd-reporting-generator/src/main/java/uk/co/bssd/reporting/generator/SimpleFileSystemGenerator.java
@@ -1,8 +1,48 @@
 package uk.co.bssd.reporting.generator;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import uk.co.bssd.reporting.adapter.CsvFileAdapter;
+import uk.co.bssd.reporting.adapter.DoubleParser;
+import uk.co.bssd.reporting.chart.ChartWriter;
+import uk.co.bssd.reporting.chart.JpegChartWriter;
+import uk.co.bssd.reporting.chart.TimeSeriesChart;
+import uk.co.bssd.reporting.chart.TimeSeriesChartDescriptor;
+import uk.co.bssd.reporting.dataset.TimedDatapoints;
+
 public class SimpleFileSystemGenerator {
 
 	public static void main(String[] args) {
-		
+		String inputDirectoryName = args[0];
+		String outputDirectoryName = args[1];
+
+		File inputDirectory = new File(inputDirectoryName);
+
+		try {
+			FileUtils.forceMkdir(new File(outputDirectoryName));
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to create output directory", e);
+		}
+
+		for (File inputFile : inputDirectory.listFiles()) {
+
+			String filename = inputFile.getName();
+			String basename = filename.substring(0, filename.lastIndexOf('.'));
+
+			CsvFileAdapter<Double> fileAdapter = new CsvFileAdapter<Double>(
+					new DoubleParser(), inputFile);
+			TimedDatapoints<Double> timedDatapoints = fileAdapter.adapt();
+
+			TimeSeriesChart<Double> chart = new TimeSeriesChart<Double>(
+					new TimeSeriesChartDescriptor(basename, true, basename,
+							"Time", "%"), timedDatapoints);
+
+			File outputFile = new File(outputDirectoryName, basename + ".jpeg");
+			ChartWriter chartWriter = new JpegChartWriter(outputFile, 640, 480);
+			chart.write(chartWriter);
+		}
 	}
 }
